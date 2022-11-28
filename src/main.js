3f805ed7369f6fad77ee395984c28794a59c403a
Amanda Holanda <amandaholanda_@hotmail.com>
2022-11-28 14:58:26 -0300

    configuracao inicial

---
+++ src/main.js
@@ -0,0 +1,39 @@
+/*import './lib/firebase.js';
+import login from './pages/login/index.js';
+import signup from './pages/signup/index.js';
+import feed from './pages/feed/index.js';
+
+const main = document.querySelector('#root');
+
+const changeHash = () => {
+  switch (window.location.hash) {
+    case '':
+      main.appendChild(login());
+      break;
+    case '#login':
+      main.appendChild(login());
+      break;
+    case '#signup':
+      main.appendChild(signup());
+      break;
+    case '#feed':
+      main.appendChild(feed());
+      break;
+    default: main.appendChild(login());
+  }
+};
+
+const executeHashChange = () => {
+  window.addEventListener('hashchange', () => {
+    main.innerHTML = '';
+    changeHash();
+  });
+};
+
+window.addEventListener('load', () => {
+  changeHash();
+  executeHashChange();
+}); */
+
+const main = document.querySelector('#root');
+main.innerHTML = "Hello World"
