3a50fc7953746610f417b2905a3c17ebae238d49
Isabella B <bellabrambilla@gmail.com>
2022-11-30 16:31:38 -0300

    filtros página tabela inter finalizados

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,16 @@
 import raizen from '../../data/raizen.js';
 
-export const filterData = (data, criteria, value) => 
-    data.filter(obj => {
-        return obj[criteria] === value
-    });
+
+
+export const filterCnpj = (data, search) =>
+    data.filter((obj) => {
+        return data.obj
+
+    }
+    
+    
+    
+    
+    )
 
 
--- src/pages/info-clients/index.js
+++ src/pages/info-clients/index.js
@@ -30,5 +30,4 @@ const info = () => {
     return container;
 }
 
-
 export default info;
