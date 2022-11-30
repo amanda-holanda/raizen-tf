a09dcd2cb1359b518fad1becc1acad5c5bde63cb
Juhreisf <109034395+Juhreisf@users.noreply.github.com>
2022-11-29 22:20:10 -0300

    construção da pagina infoclients e pagina fazenda

--- src/pages/info-clients/index.js
+++ src/pages/info-clients/index.js
@@ -1,15 +1,37 @@
 import dataSmallClients from "../../data/dataSmallClients.js";
 
 const info = () => {
-    const container = document.createElement('div');
+    const container = document.createElement('section');
+    container.setAttribute("class","info");
     const template = `
-        <h1>Info</h1>
+        <div class="header-info">
+            <!--<img src="../../img/logobandeirinha.png">-->
+            <h1>Açúcar</h1>
+        </div>
+        <div class="subtitle-info">
+            <h2>Conheça a origem desse produto</h2>
+        </div>
+        <div class="buttons-info">
+            <button class="faz-info">Fazenda</button>
+            <button class="local-info">Localidade</button>
+            <button class="certfic-info">Certificações</button>
+            <button class="curios-info">Curiosidades</button>
+        <div>
+
         <section class="smallclients-infos" id="smallClientsInfosContainer"></section>  
+   
     
     `
 
     container.innerHTML = template;
+    const fazendaInfo = container.querySelector('.faz-info');
 
+    fazendaInfo.addEventListener('click', fazendaTemplate)
+
+    function fazendaTemplate (){
+        console.log("foii")
+        window.location.hash = '#fazenda';
+    }
     const infos = dataSmallClients.infos;
 
     function displaySmallClientsInfos(infos) {
@@ -17,7 +39,7 @@ const info = () => {
             const template = `
             <div class="tabela-info-clients">   
                 <ul>                       
-                <li>Sustentabilidade: ${item.sustentabilidade}</li>               
+                <!--<li>Sustentabilidade: ${item.sustentabilidade}</li>-->              
                 </ul>
             </div>
             `;
@@ -29,6 +51,5 @@ const info = () => {
 
     return container;
 }
-
 
 export default info;
