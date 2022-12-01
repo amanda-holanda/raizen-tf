443af9c31ca8ccb1cc7ef068312faeb6ecdd20d5
Isabella B <bellabrambilla@gmail.com>
2022-12-01 12:01:56 -0300

    debug merge e função de filtro por dois períodos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -31,10 +31,13 @@ const home = () => {
             
             </div>
             <div class="periodo-container">
-                <label for="periodo">Período:
-                    <input type="date" name="periodoDe" id="firstDate" />
-                </label>                
-            </div>
+            <label for="periodo">Período:
+                <input type="date" name="periodoDe" id="firstDate"  min="1900-01-01" max="2099-09-13" />
+            </label>   
+            <label for="periodo"> à
+                <input type="date" name="periodoDe" id="secondDate"  min="1900-01-01" max="2099-09-13" />
+            </label>                       
+        </div>
         </form>
         
         <section class="bigclients-infos">
@@ -54,7 +57,7 @@ const home = () => {
             <div class="hide" id="fade"></div>
             <div class="hide" id="modal">
             <div class="modal-header">
-                <button id="close-modal">fechar</button>
+                <button id="close-modal">FECHAR</button>
             </div>
                 <div id="info-modal"></div>
             </div>
@@ -68,38 +71,22 @@ const home = () => {
     const cnpjInput = container.querySelector('#cnpjInput');
     const searchBtn = container.querySelector('#searchBtn');
     const bigClientsInfosContainer = container.querySelector('#bigClientsInfosContainer');
-    const firstDate = container.querySelector('#firstDate');
-    const dateData = infos.periodo;
+    const bigClientsDisplay = container.querySelector('.bigclients-infos');
     const infoModal = container.querySelector('#info-modal');
+    const firstDate = container.querySelector('#firstDate');
+    const secondDate = container.querySelector('#secondDate');
 
-    function displayBigClientsInfos(infos) {
+     function displayBigClientsInfos(infos) {
         const arrayBigClientsInfos = infos.map((item) => {
             const template = `  
-            <table class="tabela-home">
-            <tr class="tabela-title">
-                <tr>                                     
+                <tr>                            
                     <td class="td-cnpj">${item.cnpj}</td>
                     <td class="td-periodo">${item.periodo}</td>
                     <td class="td-lote">${item.lote}</td>
-                    <td class="td-numero">${item.fazenda.numero}</td>
+                    <td class="td-numero""><button id="btn-modal">${item.fazenda.numero}</button></td>
                     <td class="td-zona">${item.zona}</td>
                     <td class="td-talhao">${item.talhao}</td>
                     <td class="td-propriedade">${item.propriedade}</td>
-                </tr>
-            </table>                              
-            `;
-            return template;
-        });
-
-        return arrayBigClientsInfos.join("");
-    }
-
-                    <td>${item.cnpj}</td>
-                    <td>${item.lote}</td>
-                    <td><button  id="btn-modal">${item.fazenda.numero}</button></td>
-                    <td>${item.zona}</td>
-                    <td>${item.talhao}</td>
-                    <td>${item.propriedade}</td>
                 </tr>                              
             `;
             return template;
@@ -111,7 +98,7 @@ const home = () => {
     function displayModal(infos) {
         const arrayModalElement = infos.map((item) => {
             const template = `  
-               <h1>Certificações: ${item.fazenda.atributos.certificacoes}</h1>
+                <h1>Certificações:${item.fazenda.atributos.certificacoes}</h1>
                 <p>Atributos: ${item.fazenda.atributos.praticasEsg}</p>     
                 <p>Localização geográfica: ${item.localizacaoGeografica}</p>     
                                       
@@ -121,6 +108,7 @@ const home = () => {
        
         return arrayModalElement.join("");
     }    
+  
 
     function filterData(infos, cnpj, cnpjInput) {
         console.log(infos, cnpj, cnpjInput)
@@ -134,38 +122,44 @@ const home = () => {
 
     searchBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        const dataFilter = filterData(infos, 'cnpj', cnpjInput.value);
-
-        bigClientsInfosContainer.innerHTML = displayBigClientsInfos
-        (dataFilter);
-
-        const btnOpenModal = container.querySelector('#btn-modal');
-        const btnCloseModal =container.querySelector('#close-modal');
-        const modal = container.querySelector('#modal');
-        const fade = container.querySelector('#fade');
         
-        const toggleModal = () =>{
-            modal.classList.toggle('hide');
-            fade.classList.toggle('hide');
-        }
+    });
 
-        [btnOpenModal,btnCloseModal,fade].forEach((el) =>{
-            el.addEventListener('click',(e) =>{
-                toggleModal()
-                infoModal.innerHTML = displayModal(dataFilter);
-            }) 
+    searchBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        bigClientsDisplay.style.display="block";
+        const dataFilter = filterData(infos, 'cnpj', cnpjInput.value); 
+        const resultFilter = dataFilter.filter((item) => 
+        {   
+            
+            if(firstDate.value === "" && secondDate.value === ""){
+                return dataFilter
+            };
+
+            if(item.periodo >= firstDate.value && item.periodo <= secondDate.value ){
+                return item
+            }    
         })
-    });
+         
+         bigClientsInfosContainer.innerHTML = displayBigClientsInfos(resultFilter); 
 
+            const btnOpenModal = container.querySelector('#btn-modal');
+            const btnCloseModal =container.querySelector('#close-modal');
+            const modal = container.querySelector('#modal');
+            const fade = container.querySelector('#fade');
+            
+            const toggleModal = () =>{
+                modal.classList.toggle('hide');
+                fade.classList.toggle('hide');
+            }
 
-    firstDate.addEventListener('change', () => {
-        console.log(firstDate.value)
-        const dataFilter = filterData(infos, 'cnpj', cnpjInput.value);
-        const dataResult = filterData(dataFilter, 'periodo', firstDate.value);
-        console.log(dataResult)
-
-        bigClientsInfosContainer.innerHTML = displayBigClientsInfos(dataResult);
-    });
+            [btnOpenModal,btnCloseModal,fade].forEach((el) =>{
+                el.addEventListener('click',(e) =>{
+                    toggleModal()
+                    infoModal.innerHTML = displayModal(dataFilter);
+                }) 
+            })
+        });
     
     return container;
 }
