000d6356d4f6a9c889852c061c880064a19af59d
Juhreisf <109034395+Juhreisf@users.noreply.github.com>
2022-12-01 12:03:05 -0300

    ajustes na responsividade

--- src/pages/info-clients/fazenda.js
+++ src/pages/info-clients/fazenda.js
@@ -9,12 +9,12 @@ const info = () => {
             <h2>Conheça a origem desse produto</h2>
             <button class="btn-voltar">Voltar</button> 
         </div>
-            <div class="main-info">
+        <div class="main-info">
                 <img class="img-fazenda" src="img/fazenda.png" alt="fazenda">
                 <h2>Fazendas Raízen</h2>
                 <p>Na Raízen, cada fazenda é registrada com as respectivas subdivisões administrativas, zona e talhão, sendo esta segunda a maior granularidade de uma área cultivável.</p>
                 <p>O primeiro processo do cultivo é o preparo de solo, que consiste em operações mecânicas no campo, que realizará a divisão da área (fazenda) em porções menores para auxiliar na administração (fundo, zona e talhão), espelhando o definido no projeto digital (mapa). Neste processo, além da divisão da área são realizados o revolvimento de camadas superficiais do solo e a incorporação de corretivos e fertilizantes, reduzindo a compactação, elevando a permeabilidade e o armazenamento de ar e água no solo. Este processo facilita o crescimento da planta.</p>
-            </div>
+        </div>
     `
     container.innerHTML = template;
 
